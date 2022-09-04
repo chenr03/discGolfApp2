6ae7d95e10a2f3dae5de3fd6794f67875bc7ca52
Chris Henry <chrislouishenry@gmail.comgit statuscd~cd>
2022-09-04 17:08:22 -0500

    finished project

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ discGolfApp.use(express.json());
 discGolfApp.use(express.static("./src"));
 
 
-//I will capture all the Disc Golf routes we have
+// I will capture all the Disc Golf routes we have
 // in one place, instead of defining the routes directly
 // on the app object
 
@@ -20,7 +20,7 @@ discGolfApp.use(player_2_Routes);
 
 let player_3_Routes = require("./routes/player_3_Routes");
 discGolfApp.use(player_3_Routes);
-// console.log(discGolfRoutes);
+
 // console.log(player_1_DiscGolfRoutes
 // console.log(player_2_DiscGolfRoutes
 // console.log(player_3_DiscGolfRoutes
